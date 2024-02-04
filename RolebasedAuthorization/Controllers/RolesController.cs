0d06c006ee00a6640152755610d99232f114a98f
chipchippp <nvl150424@gmail.com>
2024-02-04 18:03:53 +0700

    Login Role

---
+++ RolebasedAuthorization/Controllers/RolesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RolebasedAuthorization.Controllers
+{
+    [Authorize(Roles = "admin")]
+
+    public class RolesController : Controller
+    {
+
+        private readonly RoleManager<IdentityRole> _manager;
+        public RolesController(RoleManager<IdentityRole> roleManager) {
+            _manager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            var roles = _manager.Roles;
+            return View(roles);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(IdentityRole role)
+        {
+            if (!_manager.RoleExistsAsync(role.Name).GetAwaiter().GetResult())
+            {
+                _manager.CreateAsync(new IdentityRole(role.Name)).GetAwaiter().GetResult();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
